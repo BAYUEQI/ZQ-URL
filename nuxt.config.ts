3676f19a75988faa163eda60eadc8a81ca976a5e
八月琪 <zouqi050813@gmail.com>
2025-09-17 16:10:02 +0800

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -72,7 +72,7 @@ export default defineNuxtConfig({
       production: 'runtime',
       meta: {
         title: 'Sink API',
-        description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
+        description: '',
       },
       route: '/_docs/openapi.json',
       ui: {
