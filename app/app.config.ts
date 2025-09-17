f8c518c2e7084bef31a56fdd67a53ad84bd86cfe
八月琪 <zouqi050813@gmail.com>
2025-09-17 16:09:36 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,13 +1,9 @@
 export default defineAppConfig({
   title: 'ZQ-URL',
-  email: '2839713837@qq.com',
-  github: 'https://github.com/BAYUEQI/ZQ-URL',
-  twitter: 'https://sink.cool/kai',
-  telegram: 'https://sink.cool/telegram',
-  mastodon: 'https://sink.cool/mastodon',
-  blog: 'https://sink.cool/blog',
-  description: 'A Simple / Speedy / Secure Link Shortener with Analytics, 100% run on Cloudflare.',
-  image: 'https://sink.cool/banner.png',
+  email: 'zq@qizou.dpdns.org',
+  github: 'https://github.com/BAYUEQI',
+  telegram: 'https://t.me/bayueqi',
+  blog: 'https://blog.520jacky.dpdns.org',
   previewTTL: 300, // 5 minutes
   slugRegex: /^[a-z0-9]+(?:-[a-z0-9]+)*$/i,
   reserveSlug: [
