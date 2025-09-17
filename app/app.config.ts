7eb973a2061255e5e0f05ee87cbac3b0b96f14ab
八月琪 <zouqi050813@gmail.com>
2025-09-17 18:39:41 +0800

    Update app.config.ts

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,7 +1,8 @@
 export default defineAppConfig({
   title: 'ZQ-URL',
+  description: '短链与数据分析',
   email: 'zq@qizou.dpdns.org',
-  github: 'https://github.com/BAYUEQI',
+  github: 'https://github.com/BAYUEQI/ZQ-URL',
   telegram: 'https://t.me/bayueqi',
   blog: 'https://blog.520jacky.dpdns.org',
   previewTTL: 300, // 5 minutes
